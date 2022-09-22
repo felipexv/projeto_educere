3e42836513889bb14b44f4caf26601c09be8fb49
Felipe Xavier <felipexv+github@gmail.com>
2022-09-22 17:00:26 -0300

    Portfolio

--- web/dev/autodomsystem/geradores_1/introducaoatividade.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-
-$retorno .= '<section class="slide">    
-    <div class="instrucoes">         
-        <h1>Atividade</h1>         
-        <p>'.nl2br($teoria_dados['texto1']).'</p>
-    </div> 
-</section>';
--- web/dev/autodomsystem/geradores_1/video.php
+++
@@ -1,9 +0,0 @@
-<section class="slide">
-    <h1>Introdução</h1>
-    <figure class="video">
-        <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/<?=$teoria_dados['texto1']?>?autoplay=1&title=0&byline=0&portrait=0"
-                                                                     style="position:absolute;top:87px;left:0;width:100%;height:70%;" frameborder="0"
-                                                                     webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
-        <script src="https://player.vimeo.com/api/player.js"></script>
-    </figure>
-</section>
